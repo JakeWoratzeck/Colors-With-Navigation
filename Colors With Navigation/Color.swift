8ba2d7450f571f3417a2968d7d1a7c0ffceea9f3
Jake Woratzeck <jakeworatzeck@Jakes-MacBook-Pro-4.local>
2017-10-21 13:11:45 -0500

    First commit

---
+++ Colors With Navigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors Table View
+//
+//  Created by Jake Woratzeck on 10/21/17.
+//  Copyright © 2017 Jake Woratzeck. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
